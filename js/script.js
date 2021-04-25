68c95077a8739da58f21c6f5fa6e409540a31f98
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-04-25 21:36:25 +0600

    start js

---
+++ js/script.js
@@ -0,0 +1,12 @@
+document.getElementById('root').innerHTML = " I am Md.Dulal Miah";
+document.getElementById('idh1').innerHTML = "I am looking for a Job";
+console.log('I am learning JavaScript');
+console.log("i am first line");
+console.log("I am second Line");
+document.write("Connected");
+
+
+/*Md Dulal
+Miah => Multiple line comment section => syntex*/
+prompt("Enter your age : ");
+//This command ise for input from user;
---
+++ or.js
@@ -0,0 +1,16 @@
+if(true){
+    console.log('Print it');
+}
+
+
+if(NaN){
+    console.log('Yes that is false')
+}
+
+//false , NaN , null , 0 , undefined !'anything' this is false condition
+//we can use this argument as a when we need false return
+
+/*
+when you use || that means if two condition is false than final result is false.Non other you got answer */
+console.log(true || false);
+console.log(NaN || 'Md.Dulal Miah');
