b01aa2d92b5cbfa0db45ff11e0408dcd038e8db4
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-04-26 20:36:07 +0600

    done string and logical

---
+++ operator/comparison.js
@@ -0,0 +1,6 @@
+//compare
+//return only true or false
+//use === this sign check value and type
+console.log(4 === '4');
+// > < >= <= 
+//This is also return true or false => this is boolen data type
---
+++ operator/logical.js
@@ -0,0 +1,6 @@
+// && || operator
+/* when you use || operator if one sight is true final output is true
+but if you use && operator final output is true if all is true*/
+
+console.log((4>5) || (6<7));//one condition is true
+console.log( 4 < 5 && 8 <9);
