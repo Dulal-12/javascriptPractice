cdc3b638308d738004bf985dfdb0d0eb0c498e47
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-04-26 20:20:17 +0600

    Done basic string

--- operator/assignment.js
+++ operator/assignment.js
@@ -1,5 +1,12 @@
 //operator
 //operand operator operand
-let a = 3 + 5;
+//let a = 3 + 5;
 //here 3 and 5 is operand and + is operator
-console.log(a);
+//console.log(a);
+// + - * / % this is arithmetic operator
+console.log(2 ** 3);
+
+let a = 6 , b = 5;
+let c = a++; //At first show value than increment.
+let d = ++a; //At first increment than show value.
+console.log(c);
---
+++ operator/string.js
@@ -0,0 +1,14 @@
+//string operator
+let firstName = "Md.Dulal ";
+let lastName = "Miah";
+console.log(firstName + lastName);
+
+//Number + string => string
+//string + number => string
+console.log('Hello' + 11);
+
+/* if (string - string) = string => if string number minus this work normally*/
+//Otherwise not
+
+console.log('76' - '5');
+//NaN means => not a number
