f1ec2c335ddb2977b0860c35f675aee78693b43e
Md.Dulal Miah <dulalmiah70112@gmail.com>
2021-04-27 08:01:03 +0600

    again i learn best method of string

---
+++ DataType/string.js
@@ -0,0 +1,20 @@
+//string use single / double quotesion
+//If you using single quotesion in double quotion oppsite that is no problem.
+//if using single quotesion in single or double that is problem.You have get error.
+//so we can use back slash
+console.log("Hello \" ready");
+console.log('Md \t Miah'); //big space as like as tab
+console.log('Dulal\n Miah'); //cretae new line
+
+//find length 
+console.log('Dulal'.length);
+
+//uppercase , lowercase
+
+let z = 'Programming Hero'.toLowerCase();
+console.log(z);
+
+// trim , trimStart , trimEnd , trimEnd , trimRight , trimLeft 
+//This method delete space => But not change main string => method not change main string 
+let x = ' Akash ';
+console.log(x.trim())
